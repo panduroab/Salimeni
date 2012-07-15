ab4fee9d65f186dfd06a2082bf92597fffb30d27
Abraham <panduroab@gmail.com>
2012-07-15 16:43:52 -0500

    Update master

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'admin';
-$db['default']['database'] = 'db_salimos';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
